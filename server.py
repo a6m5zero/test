08b14eab840142c1dbd4cb1ba59ec517ee2369b5
a6m5zero <vka64122@yandex.ru>
2020-07-23 01:08:25 +0300

    Test bottle server2

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 # Popitka napisat bottle server
-from bottle import route, run, view
+from bottle import route, run, view, static_file
 from datetime import datetime as dt
 from random import random
 from horoscope import generate_prophecies
+import os
 
 @route("/")
 @view("horoscope")
@@ -26,11 +27,11 @@ def api_return_predictions():
 
 @route("/static/<filename>")
 def send_static_file(filename):
-	return static_file(filename)
+	return static_file(filename,root='static')
 
 
-run(
-	host = "localhost", 
-	port=8080, 
-	debug = True
-	)
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True)
